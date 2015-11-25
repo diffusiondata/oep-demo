0c0c80c9fe4742bad2a572ca40f920b00f1a39f1
Peter Hughes <peterhughes.dev@gmail.com>
2015-11-24 19:32:59 -0800

    Make prev temps room dependent

--- HomeMonitorDemoDiffusion/src/main/java/com/push/HeatSensor.java
+++ HomeMonitorDemoDiffusion/src/main/java/com/push/HeatSensor.java
@@ -1,34 +1,37 @@
 package com.push;
 
 import java.util.Random;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.ConcurrentMap;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ScheduledExecutorService;
 import java.util.concurrent.TimeUnit;
-
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 
 import com.pushtechnology.diffusion.api.message.TopicMessage;
 import com.pushtechnology.diffusion.api.publisher.Publisher;
 import com.pushtechnology.diffusion.api.topic.Topic;
 
 public class HeatSensor implements Runnable {
-
-    private static final Logger log = LoggerFactory.getLogger(HeatSensor.class);
-    private static Random random = new Random(System.currentTimeMillis());
-    private ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
-    private double lastTemp = 15;
-    private double trend = -1;
+    private static Random RANDOM = new Random(System.currentTimeMillis());
+    
+    private final ScheduledExecutorService executor = Executors.newSingleThreadScheduledExecutor();
+    private final ConcurrentMap<String, Double> prevTemps = new ConcurrentHashMap<>();
 
     private final Publisher publisher;
     private final String[] rooms;
 
+    private volatile double trend = -1;
     private volatile int min = 10;
-    private volatile int max = 30;
-
+    private volatile int max = 30;    
+    
     public HeatSensor(Publisher publisher, String... rooms) {
         this.publisher = publisher;
         this.rooms = rooms;
+        
+        for (String room : rooms) {
+        	prevTemps.put(room, (double) 15);
+        }
+        
         executor.scheduleAtFixedRate(this, 0, 1, TimeUnit.SECONDS);
     }
 
@@ -40,11 +43,12 @@ public class HeatSensor implements Runnable {
         try {
             for (String room : rooms) {
                 if (!room.equals("Outside")) {
-                    Double v = readValue();
-                    Topic topic = publisher.getTopic("Sensors/Readings/Temp/" + room);
+                    final Double value = readValue(room);
+                    final Topic topic = publisher.getTopic("Sensors/Readings/Temp/" + room);
 
-                    TopicMessage message = topic.createDeltaMessage();
-                    message.putRecord(v.toString());
+                    final TopicMessage message = topic.createDeltaMessage();
+                    message.putRecord(value.toString());
+                    
                     publisher.publishMessage(message);
                 }
             }
@@ -55,10 +59,10 @@ public class HeatSensor implements Runnable {
 
     public void heaterOn(boolean on) {
         if (on) {
-            log.debug("Heating on");
+            System.out.println("Heating on");
             trend = 1;
         } else {
-            log.debug("Heating off");
+        	System.out.println("Heating off");
             trend = -1;
         }
     }
@@ -68,10 +72,11 @@ public class HeatSensor implements Runnable {
         this.max = max;
     }
 
-    public double readValue() {
-        double variation = random.nextInt(50) / 10d;
-
-        double newTemp = lastTemp + (trend * variation);
+    public double readValue(String room) {
+        final double variation = RANDOM.nextInt(50) / 10d;
+        final double prev = prevTemps.get(room);
+        
+        double newTemp = prev + (trend * variation);
 
         if (newTemp < min) {
             newTemp = min;
@@ -79,10 +84,8 @@ public class HeatSensor implements Runnable {
             newTemp = max;
         }
 
-        if (random.nextInt(max) == 1) {
-            lastTemp = newTemp;
-        }
+        prevTemps.replace(room, prev, newTemp);   
+        
         return newTemp;
     }
-
 }
